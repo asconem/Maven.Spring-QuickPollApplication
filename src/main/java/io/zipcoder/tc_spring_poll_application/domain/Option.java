0e86f096eb0b620c9897264c6ce8189e23d2cdd4
asconem <asconem@gmail.com>
2020-04-03 19:02:31 -0400

    Adding Option class

--- src/main/java/io/zipcoder/tc_spring_poll_application/domain/Option.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Option.java
@@ -12,4 +12,25 @@ public class Option {
     @GeneratedValue
     @Column(name = "OPTION_ID")
     private Long id;
+    @Column(name = "OPTION_VALUE")
+    private String value;
+
+    public Option() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
